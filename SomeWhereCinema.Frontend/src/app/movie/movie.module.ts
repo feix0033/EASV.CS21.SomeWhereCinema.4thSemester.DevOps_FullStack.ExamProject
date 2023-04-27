066c04cd20ced779d14fe9fe90fa4acdb8e51775
feix0033 <feix0033@easv365.dk>
2023-04-27 14:33:15 +0200

    make the basic structure for this front end application.

---
+++ SomeWhereCinema.Frontend/src/app/movie/movie.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MovieRoutingModule } from './movie-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MovieRoutingModule
+  ]
+})
+export class MovieModule { }
---
+++ SomeWhereCinema.Frontend/src/app/user/user-login/user-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.scss']
+})
+export class UserLoginComponent {
+
+}
