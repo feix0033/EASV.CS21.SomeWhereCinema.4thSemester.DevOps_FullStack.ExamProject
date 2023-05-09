e24ca226bb8a5a681f645999cf6ad2df1b291fe7
feix0033 <feix0033@easv365.dk>
2023-05-09 09:50:04 +0200

    create dbcontext and dbset

--- SomeWhereCinema.Backend/SomeWhereCinema.DataAccess/MainDbContext.cs
+++ SomeWhereCinema.Backend/SomeWhereCinema.DataAccess/MainDbContext.cs
@@ -1,10 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using SomeWhereCinema.Core.Models;
+using SomeWhereCinema.DataAccess.Entities;
 
 namespace SomeWhereCinema.DataAccess;
 
 public class MainDbContext:DbContext
 {
+    public virtual DbSet<MovieEntity>? Movies { get; set; }
+
     public MainDbContext(DbContextOptions<MainDbContext>options) : base(options)
     {
     }
+
+    
 }
--- SomeWhereCinema.Backend/SomeWhereCinema.UnitTest/SomeWhereCinema.Infrastructure.Test/SomeWhereCinema.DataAccess.Test/MainDbContextTest.cs
+++ SomeWhereCinema.Backend/SomeWhereCinema.UnitTest/SomeWhereCinema.Infrastructure.Test/SomeWhereCinema.DataAccess.Test/MainDbContextTest.cs
@@ -1,14 +1,31 @@
 using EntityFrameworkCore.Testing.Moq;
+using Microsoft.EntityFrameworkCore;
+using SomeWhereCinema.Core.Models;
 using SomeWhereCinema.DataAccess;
+using SomeWhereCinema.DataAccess.Entities;
 
 namespace SomeWhereCinema.UnitTest.SomeWhereCinema.Infrastructure.Test.SomeWhereCinema.DataAccess.Test;
 
 public class MainDbContextTest
 {
+    private readonly MainDbContext _mockedDbContext;
+
+    public MainDbContextTest()
+    {
+         _mockedDbContext = Create.MockedDbContextFor<MainDbContext>();
+    }
+
     [Fact]
     public void DbContext_whitDbContextOption_IsAvailable()
     {
-        var mockedDbContext = Create.MockedDbContextFor<MainDbContext>();
-        Assert.NotNull(mockedDbContext);
+        // var mockedDbContext = Create.MockedDbContextFor<MainDbContext>();
+        Assert.NotNull(_mockedDbContext);
+    }
+
+    [Fact]
+    public void DbContext_DbSets_MustHaveDbSetWithTypeMovieEntity()
+    {
+        // var mockedDbContext = Create.MockedDbContextFor<MainDbContext>();
+        Assert.True(_mockedDbContext.Movies is DbSet<MovieEntity>);
     }
 }
