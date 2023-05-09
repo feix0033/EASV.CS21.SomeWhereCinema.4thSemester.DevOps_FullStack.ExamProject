1eb71490e4e1f3d14d9545025ac6b6588d6907a0
feix0033 <feix0033@easv365.dk>
2023-05-09 14:46:57 +0200

    make tdd about controller method

--- SomeWhereCinema.Backend/SomeWhereCinema.UnitTest/SomeWhereCinema.Infrastructure.Test/SomeWhereCinema.WebApi.Test/MovieControllerTest.cs
+++ SomeWhereCinema.Backend/SomeWhereCinema.UnitTest/SomeWhereCinema.Infrastructure.Test/SomeWhereCinema.WebApi.Test/MovieControllerTest.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
+using SomeWhereCinema.Core.Models;
 using SomeWhereCinema.WebApi.Controllers;
 
 namespace SomeWhereCinema.UnitTest.SomeWhereCinema.Infrastructure.Test.SomeWhereCinema.WebApi.Test;
@@ -23,5 +24,61 @@ public class MovieControllerTest
                         .GetType()
                         .Name
                         .Equals("ApiControllerAttribute")));
+    }
+
+    [Fact]
+    public void MovieController_UseRouteAttribute_WithParamApiControllerNameRoute()
+    {
+        var attribute = typeof(MovieController)
+            .GetTypeInfo()
+            .GetCustomAttributes()
+            .FirstOrDefault(attr => attr
+                .GetType()
+                .Name
+                .Equals("RouteAttribute"));
+        var routeAttr = attribute as RouteAttribute;
+        
+        Assert.NotNull(attribute);
+        Assert.Equal("api/[controller]", routeAttr?.Template);
+    }
+
+    [Fact]
+    public void MovieController_HasGetAllMethod()
+    {
+        Assert.NotNull(
+            typeof(MovieController)
+            .GetMethods()
+            .FirstOrDefault(method => "GetAll".Equals(method.Name))
+        );
+    }
+
+    [Fact]
+    public void MovieController_HasGetAllMethod_IsPublic()
+    {
+        Assert.True(
+            typeof(MovieController)
+                .GetMethods()
+                .FirstOrDefault(method => "GetAll".Equals(method.Name))!
+                .IsPublic
+        );
+    }
+    
+    [Fact]
+    public void MovieController_HasGetAllMethod_ReturnsListOfProductsInActionResult()
+    {
+        Assert.Equal(
+            typeof(ActionResult<List<Movie>>)
+                .FullName,
+            typeof(MovieController)
+                .GetMethods()
+                .FirstOrDefault(method => "GetAll".Equals(method.Name))!
+                .ReturnType
+                .FullName
+        );
+    }
+    [Fact]
+    public void MovieController_GetAllMethod_HasGetHttpAttribute()
+    {
+        
     }
 }
--- SomeWhereCinema.Backend/SomeWhereCinema.WebApi/Controllers/MovieController.cs
+++ SomeWhereCinema.Backend/SomeWhereCinema.WebApi/Controllers/MovieController.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using SomeWhereCinema.Core.Models;
 
 namespace SomeWhereCinema.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class MovieController:ControllerBase
-{
-    
+{ 
+    [HttpGet]
+    public ActionResult<List<Movie>> GetAll()
+    {
+        return null;
+    }
 }
